654ec8c1a64d130335aaf8f250d55da60eb8bfd7
Bartłomiej Juda <bartekjkk@gmail.com>
2021-11-26 14:42:35 +0700

    django commit

--- base/models.py
+++ base/models.py
@@ -8,8 +8,8 @@ from django.db.models.deletion import CASCADE
 class Topic(models.Model):
     name= models.CharField(max_length=200)
 
-    def __str__(self) -> str:
-        return super().__str__()
+    def __str__(self):
+        return self.name
 
 
 class Room(models.Model):
