fd31de927f3d018be47087f09166f087a3fd0bce
Bartłomiej Juda <bartekjkk@gmail.com>
2021-11-30 14:15:46 +0700

    django

--- base/views.py
+++ base/views.py
@@ -12,7 +12,7 @@ from .forms import RoomForm
 
 def home(request):
     q= request.GET.get('q') if request.GET.get('q') !=None else ''
-    rooms= Room.objects.filter(topic__name__icontsains=q)
+    rooms= Room.objects.filter(topic__name__icontains=q)
     topics = Topic.objects.all()
     context= {'rooms': rooms,'topics': topics}
     return render(request, 'base/home.html',context)
