0cc177adb5de656e40ba82a2e97080ff44df2f5a
Bartłomiej Juda <bartekjkk@gmail.com>
2021-11-30 14:40:15 +0700

    django commit

--- base/views.py
+++ base/views.py
@@ -17,7 +17,8 @@ def home(request):
     rooms= Room.objects.filter(
         Q(topic__name__icontains=q) | 
         Q(name__icontains=q) |
-        Q(description__icontains=q)
+        Q(description__icontains=q) |
+        Q(host__username__icontains =q)
         )
     topics = Topic.objects.all()
     context= {'rooms': rooms,'topics': topics}
