3f5db0aa3d47b0dc65baa808b867c83509147948
Bartłomiej Juda <bartekjkk@gmail.com>
2021-12-02 14:50:01 +0700

    django

--- base/views.py
+++ base/views.py
@@ -79,10 +79,13 @@ def createRoom(request):
     context = {'form':form}
     return render(request, 'base/room_form.html',context)
 
+@login_required(login_url='login')
 def updateRoom(request,pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
-    
+    if request.user != room.host: 
+        return HttpResponse('You are not allowed here!')
+
 
     if request.method == 'POST':
         form = RoomForm(request.POST, instance=room)
@@ -93,6 +96,7 @@ def updateRoom(request,pk):
     context = {'form':form}
     return render(request,'base/room_form.html',context)
 
+@login_required(login_url='login')
 def deleteRoom(request,pk):
     room= Room.objects.get(id=pk)
     if request.method == "POST":
