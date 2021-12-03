fea7e82e03b54ce4b225995053a836c6723e2a9b
Bartłomiej Juda <bartekjkk@gmail.com>
2021-12-03 13:04:39 +0700

    django messages

--- base/views.py
+++ base/views.py
@@ -80,7 +80,7 @@ def room(request,pk):
    
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
-    context = {'room': room, 'room_essages':room_messages }
+    context = {'room': room, 'room_messages':room_messages }
     return render(request, 'base/room.html',context)
 
 
