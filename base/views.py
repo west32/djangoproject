0b2c58c8924acbdda8c3d6804fe6adc786cd1398
Bartłomiej Juda <bartekjkk@gmail.com>
2021-12-04 22:13:12 +0700

    django messages

--- base/views.py
+++ base/views.py
@@ -127,11 +127,27 @@ def updateRoom(request,pk):
 @login_required(login_url='login')
 def deleteRoom(request,pk):
     room= Room.objects.get(id=pk)
+
+    if request.user != room.host:
+        return HttpResponse('You are not allowed here!')
+
     if request.method == "POST":
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': room})
 
+
+@login_required(login_url='login')
+def deleteMessage(request,pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here!')
+
+    if request.method == "POST":
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': message})
 
 def sztoki(request):
     return render(request, 'base/sztoki.html')
